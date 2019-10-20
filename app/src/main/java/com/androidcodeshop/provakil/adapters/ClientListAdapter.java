29842f7308e73e27be0b868b51759b8c5c00e785
minielectron <prakash009Kp@gmail.com>
2019-10-20 13:59:45 +0530

    Options menu is added to list

--- app/src/main/java/com/androidcodeshop/provakil/adapters/ClientListAdapter.java
+++ app/src/main/java/com/androidcodeshop/provakil/adapters/ClientListAdapter.java
@@ -2,11 +2,14 @@ package com.androidcodeshop.provakil.adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.androidcodeshop.provakil.R;
@@ -39,6 +42,35 @@ public class ClientListAdapter extends RecyclerView.Adapter<ClientListAdapter.Vi
         holder.nameTv.setText(clientDetailsList.get(position).getName());
         holder.clientCodeTv.setText(String.format(Locale.ENGLISH, "Client Code : %s", clientDetailsList.get(position).getmClientCode()));
         holder.phoneNumberTv.setText(String.format(Locale.ENGLISH, "Mobile No : %s", clientDetailsList.get(position).getmContactNumber()));
+
+        holder.menuTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(mContext, holder.menuTv);
+                popup.inflate(R.menu.options_menu);
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_view:
+                                Toast.makeText(mContext, "View Clicked", Toast.LENGTH_SHORT).show();
+                                return true;
+                            case R.id.action_edit:
+                                Toast.makeText(mContext, "Edit Clicked", Toast.LENGTH_SHORT).show();
+
+                                return true;
+                            case R.id.action_delete:
+                                Toast.makeText(mContext, "Delete Clicked", Toast.LENGTH_SHORT).show();
+
+                                return true;
+                            default:
+                                return false;
+                        }
+                    }
+                });
+                popup.show();
+            }
+        });
     }
 
     @Override
@@ -53,6 +85,8 @@ public class ClientListAdapter extends RecyclerView.Adapter<ClientListAdapter.Vi
         TextView clientCodeTv;
         @BindView(R.id.phone_number_tv)
         TextView phoneNumberTv;
+        @BindView(R.id.optionsMenuTv)
+        TextView menuTv;
 
         ViewHolder(View view) {
             super(view);
